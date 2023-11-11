5248b95aaf6c6878895de28b14360ff1a6f08236
Marski <kontakt@marski.pl>
2023-11-11 16:26:09 +0100

    View

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -11,24 +11,43 @@ class PostController extends AbstractController
     #[Route('/', name: 'posts.index', methods: ['GET'])]
     public function index(): Response
     {
-        return new Response(
-            '<h1>List of all posts here <br> Named route that we will use in the view: ' . $this->generateUrl('posts.index') . '</h1>'
-        );
+        return $this->render('post/index.html.twig');
+    }
+
+    #[Route('/post/new', name: 'posts.new', methods: ['GET', 'POST'])]
+    public function new(): Response
+    {
+        return $this->render('post/new.html.twig');
+    }
+
+    #[Route('/post/show/{id}', name: 'posts.show', methods: ['GET'])]
+    public function show($id): Response
+    {
+        return $this->render('post/show.html.twig');
+    }
+
+    #[Route('/post/{id}/edit', name: 'posts.edit', methods: ['GET', 'POST'])]
+    public function edit($id): Response
+    {
+        // return $this->redirectToRoute('posts.index');
+        return $this->render('post/edit.html.twig');
+    }
+
+    #[Route('/post/{id}/delete', name: 'posts.delete', methods: ['POST'])]
+    public function delete($id): Response
+    {
+        return new Response('delete post from the database');
     }
 
     #[Route('/posts/user/{id}', name: 'posts.user', methods: ['GET'])]
     public function user($id): Response
     {
-        return new Response(
-            '<h1>List of posts from specific user <br> User id: ' . $id . '<br> Named route that we will use in the view: ' . $this->generateUrl('posts.user', ['id' => $id]) . '</h1>'
-        );
+        return $this->render('post/index.html.twig');
     }
 
     #[Route('/toggleFollow/{user}', name: 'toggleFollow', methods: ['GET'])]
     public function toggleFollow($user): Response
     {
-        return new Response(
-            '<h1>Togle like/dislike<br> User id: ' . $user . '<br> Named route that we will use in the view: ' . $this->generateUrl('toggleFollow', ['user' => $user]) . '</h1>'
-        );
+        return new Response('logic for toggling like/dislike');
     }
 }
