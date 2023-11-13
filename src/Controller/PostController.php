861c4d0b3170a0add0751330b1cd10e66dbdf309
Marski <kontakt@marski.pl>
2023-11-13 16:40:45 +0100

    Routing default language for subpages

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -6,46 +6,47 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', requirements: ['_locale' => 'en|pl'])]
 class PostController extends AbstractController
 {
-    #[Route('/{_locale?}', name: 'posts.index', methods: ['GET'])]
-    public function index(): Response
+    #[Route('/{_locale}', name: 'posts.index', methods: ['GET'])]
+    public function index(string $_locale = 'en'): Response
     {
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/post/new', name: 'posts.new', methods: ['GET', 'POST'])]
+    #[Route('/{_locale}/post/new', name: 'posts.new', methods: ['GET', 'POST'])]
     public function new(): Response
     {
         return $this->render('post/new.html.twig');
     }
 
-    #[Route('/post/{id}/{_locale?}', name: 'posts.show', methods: ['GET'])]
+    #[Route('/{_locale}/post/{id}/', name: 'posts.show', methods: ['GET'])]
     public function show($id): Response
     {
         return $this->render('post/show.html.twig');
     }
 
-    #[Route('/post/{id}/edit', name: 'posts.edit', methods: ['GET', 'POST'])]
+    #[Route('/{_locale}/post/{id}/edit', name: 'posts.edit', methods: ['GET', 'POST'])]
     public function edit($id): Response
     {
         // return $this->redirectToRoute('posts.index');
         return $this->render('post/edit.html.twig');
     }
 
-    #[Route('/post/{id}/delete', name: 'posts.delete', methods: ['POST'])]
+    #[Route('/{_locale}/post/{id}/delete', name: 'posts.delete', methods: ['POST'])]
     public function delete($id): Response
     {
         return new Response('delete post from the database');
     }
 
-    #[Route('/posts/user/{id}', name: 'posts.user', methods: ['GET'])]
+    #[Route('/{_locale}/posts/user/{id}', name: 'posts.user', methods: ['GET'])]
     public function user($id): Response
     {
         return $this->render('post/index.html.twig');
     }
 
-    #[Route('/toggleFollow/{user}', name: 'toggleFollow', methods: ['GET'])]
+    #[Route('/{_locale}/toggleFollow/{user}', name: 'toggleFollow', methods: ['GET'])]
     public function toggleFollow($user): Response
     {
         return new Response('logic for toggling like/dislike');
